46a6a1eab2921e5190f2e63c7abb2e4550be9a5b
walid yusuf <walid-yusuf356>
2024-02-02 01:45:15 -0600

    I added new features to Netflix-Clone

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import Nav from './Nav';
 import requests from './requests';
 import Row from './Row';
 import React from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App ">
       <Nav/>
+      <Header />
       <Banner />
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} /> 
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
       <Row title="Documentaries Movies" fetchUrl={requests.fetchDocumentaries} />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import XIcon from '@mui/icons-material/X';
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className='footer_outer_container'>
+      <div className='footer_inner_container'>
+        <div className='footer_icons'>
+          <FacebookOutlinedIcon />
+          <InstagramIcon />
+          <YouTubeIcon />
+          <XIcon />
+        </div>
+        <div className='footer_data'>
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li>Legal Notice</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+        <div className='footer_service_code'>
+            <p>Service Code</p>
+        </div>
+        <div className='footer_copy_right'>
+            © 1997-2021 Netflix, Inc. 
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
+
+
+
+
